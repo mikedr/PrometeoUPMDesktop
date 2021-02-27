e4f0e60f64ff3e2152ddac5685b5ca2c475e400b
mikedr <derosamiguel@gmail.com>
2021-02-27 16:57:29 -0300

    Agrego el menu y sus opciones

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,10 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 
 public class MainFrame extends JFrame{
 	
@@ -22,7 +28,51 @@ public class MainFrame extends JFrame{
 	private void addComponents() {
 		add(resultsContainer,BorderLayout.WEST);
 		add(textPanel,BorderLayout.CENTER);
-		add(toolbar,BorderLayout.NORTH);		
+		add(toolbar,BorderLayout.NORTH);
+		setJMenuBar(createMenuBar());
+	}
+
+	private JMenuBar createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu medicionesMenu = new JMenu("Mediciones");
+		JMenu dispositivoMenu = new JMenu("Dispositivo");
+		JMenu aplicacionMenu = new JMenu("Prometeo UPM");
+		
+		JMenuItem importarMenuItem = new JMenuItem("Importar mediciones");
+		JMenu exportarMenuItem = new JMenu("Exportar resultados");
+		JMenuItem medicionesXlsMenuItem = new JMenuItem("Mediciones");
+		JMenuItem reporteMenuItem = new JMenuItem("Reporte");
+		JMenuItem aboutMenuItem = new JMenuItem("Acerca de");
+		JMenuItem salirMenuItem = new JMenuItem("Salir");
+
+		exportarMenuItem.add(medicionesXlsMenuItem);
+		exportarMenuItem.add(reporteMenuItem);
+		
+		medicionesMenu.add(importarMenuItem);
+		medicionesMenu.add(exportarMenuItem);
+		
+		aplicacionMenu.add(aboutMenuItem);
+		aplicacionMenu.addSeparator();
+		aplicacionMenu.add(salirMenuItem);
+
+		JMenuItem gestionarMenuItem = new JMenuItem("Gestionar");
+		dispositivoMenu.add(gestionarMenuItem);
+
+		salirMenuItem.setMnemonic(KeyEvent.VK_I);
+		salirMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+
+		salirMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);	
+			}
+		});
+		
+		menuBar.add(medicionesMenu);
+		menuBar.add(dispositivoMenu);
+		menuBar.add(aplicacionMenu);
+		
+		return menuBar;
 	}
 
 	private void instaciateComponents() {
