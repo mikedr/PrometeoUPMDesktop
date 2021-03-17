bd4fe68da6caf51a15fc471c81af18460812cb21
mikedr <derosamiguel@gmail.com>
2021-03-16 21:04:16 -0300

    Funcionalidad de leer archivo y cargar tabla

--- src/model/Database.java
+++ src/model/Database.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Database {
 
@@ -26,14 +29,50 @@ public class Database {
 	public void loadFromFile(File file) throws IOException {
 		FileReader fr = new FileReader(file);
 		BufferedReader br = new BufferedReader(fr); 
-		StringBuffer sb = new StringBuffer();
 		String line;
-		while((line=br.readLine()) != null) {  
-			sb.append(line);
-			sb.append("\n");   
+		while((line = br.readLine()) != null) {  
+			parseLine(line);
 		}
 		fr.close();
-		System.out.println(sb.toString());
-		System.out.println("Contents of File: "); 
 	}
+
+	private void parseLine(String line) {
+		StringTokenizer st;
+		String controlField, idField, batteryField, tempField, timeField, sanitizedTime;
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_TIME;
+		LocalTime tiempo;
+		Float temperatura;
+		Measurement measurement;
+		st = new StringTokenizer(line,"/");
+		controlField = st.nextToken();
+		if(controlField.equals(new String("INF"))) {
+			try {
+				idField = st.nextToken();
+			    batteryField = st.nextToken();
+			    tempField = st.nextToken();
+			    temperatura = Float.parseFloat(tempField);
+			    timeField = st.nextToken();
+			    sanitizedTime = timeSanitizer(timeField);
+			    tiempo = LocalTime.parse(sanitizedTime, dateTimeFormatter);
+			    measurement = new Measurement(tiempo, temperatura);
+			    measurements.add(measurement);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	 private String timeSanitizer(String timeToSanitize) {
+		int dotIndex = timeToSanitize.indexOf('.');
+		if (dotIndex > 0) {
+		    String temporalString = timeToSanitize.substring(0, dotIndex);
+		    StringBuilder fixedTime = new StringBuilder(temporalString);
+		    if(temporalString.length()==7) {
+		    	fixedTime.insert(temporalString.length()-1, '0');
+		    }
+		    return fixedTime.toString();
+		} else {
+			return timeToSanitize;
+		}
+	 }
 }
