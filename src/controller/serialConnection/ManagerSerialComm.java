67cc92cb2cb9cfe5ee2edd43a03ba03f72a26080
mikedr <derosamiguel@gmail.com>
2021-04-08 23:46:43 -0300

    Refactoring

--- src/controller/serialConnection/ManagerSerialComm.java
+++ src/controller/serialConnection/ManagerSerialComm.java
@@ -7,14 +7,13 @@ import javax.comm.CommPortIdentifier;
 public class ManagerSerialComm {
 	
 	private CommPortIdentifier portId;
-	private Enumeration ports = null;
+	private Enumeration ports;
 	private static final String MESSAGE_START_TEST_PORT = "Abrimos hilo para testear puerto ";
 	
 	public void setConection() {
 	    ports  = CommPortIdentifier.getPortIdentifiers();
-	    CommPortIdentifier portId = null;
         while (ports.hasMoreElements()) {
-             portId = (CommPortIdentifier) ports.nextElement();
+            portId = (CommPortIdentifier) ports.nextElement();
             System.out.println(MESSAGE_START_TEST_PORT+portId.getName());
             Thread newThread = new Thread(new PortTester(portId));
             newThread.start();
